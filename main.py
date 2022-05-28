b450a1c6143d6b2c539e3308da0b09f9f8146d29
RejoiceRejoice <105215752+RejoiceRejoice@users.noreply.github.com>
2022-05-28 22:39:32 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if (sorted(word) == sorted(anagram)):
+        print(sorted(word), sorted(anagram))
+        return True
+    return False    
+
+print(find_anagram('hello', 'check'))
+
+def find_anagram(word, anagram):
+    if (sorted(word) == sorted(anagram)):
+        print(sorted(word), sorted(anagram))
+        return True
+    return False    
+
+print(find_anagram('below', 'elbow'))
